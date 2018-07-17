947082d7d7a18acde7bf2dda8d079e9abd1d1774
Nat! <nat@mulle-kybernetik.com>
2018-07-18 00:41:30 +0200

    rename cmake file so its properly included

---
+++ src/foundation/objc-cmake/project/all/cmake/share/EnvironmentAux.cmake
@@ -0,0 +1,11 @@
+if( NOT __ENVIRONMENT_AUX_CMAKE__)
+   set( __ENVIRONMENT_AUX_CMAKE__ ON)
+
+   if( MULLE_TRACE_INCLUDE)
+      message( STATUS "# Include \"${CMAKE_CURRENT_LIST_FILE}\"" )
+   endif()
+
+   set( CMAKE_FIND_FRAMEWORK "LAST")
+
+   include( ExecutableObjCAux OPTIONAL)
+endif()
